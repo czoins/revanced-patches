98a945e9bf2f5093d2c9ac76744d4f7524d0cc79
czoins <72418386+czoins@users.noreply.github.com>
2025-03-15 02:52:29 +0100

    Update RemoveScreenCaptureRestrictionPatch.java

--- extensions/all/misc/screencapture/remove-screen-capture-restriction/src/main/java/app/revanced/extension/all/screencapture/removerestriction/RemoveScreenCaptureRestrictionPatch.java
+++ extensions/all/misc/screencapture/remove-screen-capture-restriction/src/main/java/app/revanced/extension/all/screencapture/removerestriction/RemoveScreenCaptureRestrictionPatch.java
@@ -8,7 +8,7 @@ import androidx.annotation.RequiresApi;
 @SuppressWarnings("unused")
 public final class RemoveScreenCaptureRestrictionPatch {
     // Member of AudioAttributes.Builder
-    @RequiresApi(api = Build.VERSION_CODES.Q)
+
     public static AudioAttributes.Builder setAllowedCapturePolicy(final AudioAttributes.Builder builder, final int capturePolicy) {
         builder.setAllowedCapturePolicy(AudioAttributes.ALLOW_CAPTURE_BY_ALL);
 
